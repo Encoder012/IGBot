5b41c1c2eb12572644709fc9a744999413f3c4ad
Encoder012 <49637067+Encoder012@users.noreply.github.com>
2025-06-11 19:36:43 +0530

    change reel downloader

--- utils/igReelDLD.js
+++ utils/igReelDLD.js
@@ -1,8 +1,13 @@
 
 const axios = require('axios');
-async function igReelDLD(url) {
-    const resp = await axios.get(`https://instagram-reels-downloader-tau.vercel.app/api/video?postUrl=${url}`)
-    const videoUrl = resp.data.data.videoUrl;
+async function igReelDLD(igUrl) {
+    const igUrl = "https://www.instagram.com/reel/DHQGCdTR4Yg/?igsh=OHNwbG92djQwMDNj"
+    const reelId = igUrl.split('/reel/')[1].split('/')[0];
+    console.log("Reel Id: ", reelId)
+    // const resp = await axios.get(`https://instagram-reels-downloader-tau.vercel.app/api/video?postUrl=${url}`)
+    // const videoUrl = resp.data.data.videoUrl;
+    const resp = await axios.get(`"https://gram-grabberz.vercel.app/api/instagram/p/${reelId}`);
+    const videoUrl = resp.data.xdt_shortcode_media.video_url
     return videoUrl;
 
 
